b1c030079e4525a3d6d720d5cdfc7a8c4808c080
shelley <shelleywu1921@gmail.com>
2021-03-27 13:42:34 -0400

    initial draft of code

---
+++ README.md
@@ -0,0 +1,2 @@
+References:
+https://stackoverflow.com/questions/28046084/chrome-app-extension-to-remove-a-page-element
---
+++ content_script.js
@@ -0,0 +1,5 @@
+var element = document.querySelector('div.ytd-two-column-browse-results-renderer');
+element.parentElement.removeChild(element);
+
+
+
